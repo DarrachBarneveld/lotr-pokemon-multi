6740baa8505706dad9b10ad1419871c72c95af3d
Darrach Barneveld <darrachb1991@gmail.com>
2023-04-11 21:50:39 +0100

    added dynamic viewport height

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import Navbar from "./components/Navbar";
 import { useEffect, useState } from "react";
-import { IPlayer, IRoom } from "./models";
+import { IRoom } from "./models";
 
 import Lobby from "./components/Lobby";
 import { socket } from "./config/socket";
 import FightZone from "./components/FightZone";
-import data from "./assets/data/characters.json";
-import FlipCard from "./components/FlipCard";
-
-type RoomData = {
-  room: IRoom;
-  player: IPlayer;
-};
 
 function App() {
   const [room, setRoom] = useState<IRoom | undefined>();
--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -4,7 +4,7 @@ import { socket } from "../config/socket";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { IRoom } from "../models";
-import { faHouse, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faSearch, faUsers } from "@fortawesome/free-solid-svg-icons";
 
 interface LobbyProps {}
 
@@ -38,39 +38,51 @@ const Lobby: FC<LobbyProps> = ({}) => {
   };
 
   return (
-    <div className="flex flex-1 justify-center items-center">
-      <div className="flex flex-col h-[600px] pt-0 overflow-scroll items-center max-w-5xl rounded-xl space-y-4 bg-slate-100 border-2 border-slate-800 ">
+    <div className="flex flex-1 justify-center items-center p-2">
+      <div className="flex flex-col h-[600px] pt-0 overflow-scroll items-center w-full max-w-xl rounded-xl space-y-4 bg-slate-100 border-2 border-slate-800 ">
         <div className="bg-slate-800 w-full text-lg space-y-2">
-          <div className=" relative flex justify-center m-1">
+          <div className="relative flex justify-evenly p-1 md:justify-center">
             <h3 className="text-3xl font-extrabold uppercase text-slate-50">
               Lobby
             </h3>
-            <div className="absolute top-0 right-0 flex  space-x-1">
-              <div className="space-x-3 bg-green-500 px-1 rounded-lg font-bold text-slate-50">
+            <div className="flex justify-evenly md:absolute top-0 right-0 space-x-1">
+              <div className="h-fit space-x-3 bg-green-500 px-1 rounded-lg font-bold text-slate-50">
                 <FontAwesomeIcon icon={faHouse} />
                 <span>{allRooms.length}</span>
               </div>
-              <div className=" space-x-3 bg-green-500 px-1 rounded-lg font-bold text-slate-50">
+              <div className="h-fit space-x-3 bg-green-500 px-1 rounded-lg font-bold text-slate-50">
                 <FontAwesomeIcon icon={faUsers} />
                 <span>{totalUsers}</span>
               </div>
             </div>
           </div>
           <div className="flex items-center justify-center space-x-3 p-2  bg-slate-800 w-full border-b border-slate-50">
-            <button className="btn" onClick={handleSearchRoom}>
-              Search Room
+            <button
+              className="btn text-slate-50 space-x-1"
+              onClick={handleSearchRoom}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+              <span>Search</span>
             </button>
-            <button className="btn" onClick={() => handleJoinRoom()}>
-              Join Room
+            <button
+              className="btn text-slate-50 space-x-1"
+              onClick={() => handleJoinRoom()}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+              <span>Join</span>
             </button>
-            <button className="btn" onClick={handleCreateRoom}>
-              Create Room
+            <button
+              className="btn text-slate-50 space-x-1"
+              onClick={handleCreateRoom}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+              <span>Create</span>
             </button>
           </div>
         </div>
         <div className="p-2 space-y-2 w-full">
           {allRooms.length == 0 && (
-            <div className="flex flex-col items-center bg-slate-800 w-[500px] p-4 text-center text-slate-50 space-y-2 rounded-xl">
+            <div className="flex flex-col items-center bg-slate-800 w-full py-5 px-2 text-center text-slate-50 space-y-2 rounded-xl">
               <h2 className="text-2xl">No Rooms Available</h2>
               <button className="btn w-fit" onClick={handleCreateRoom}>
                 Create Room
